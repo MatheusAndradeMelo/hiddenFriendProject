6b90387b9a81ea5dee86148cb05ceae2692b919c
MatheusAndradeMelo <mandrademelo77@gmail.com>
2024-11-04 20:24:40 -0300

    feat: create home component standalone and make app component standalone too so i remove app module because we dont need more

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { HomeComponent } from './app/components/home/home.component';
+import { provideRouter } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// Usando bootstrapApplication para inicializar o AppComponent
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', component: HomeComponent } // Define a rota inicial
+    ])
+  ]
+}).catch(err => console.error(err));
