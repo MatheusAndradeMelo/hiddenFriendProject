c9bcba0ed6b69baa4b929d8c7a28ea2f87a73981
MatheusAndradeMelo <mandrademelo77@gmail.com>
2024-11-05 22:09:04 -0300

    feat: add label and mat input and add some new style to form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [MatFormFieldModule,MatInputModule, FormsModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
